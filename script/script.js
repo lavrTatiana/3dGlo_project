e993d431796b527a9e70d89a5cbce7a2e4828688
Tatiana Lavrovskaya <lavr39820@gmail.com>
2020-05-03 21:55:07 +0300

    first part

--- script/script.js
+++ script/script.js
@@ -273,33 +273,75 @@ window.addEventListener('DOMContentLoaded', function(){
     const commandPhoto = document.querySelectorAll('.command__photo');
 
     commandPhoto.forEach((item) => {
+      let anotherImg;
       item.addEventListener('mouseenter', (event) => {
-        event.target.alt = event.target.src;
+        anotherImg = event.target.src;
         event.target.src = event.target.dataset.img;
       });
 
       item.addEventListener('mouseleave', (event) => {
-        event.target.src = event.target.alt;
+        event.target.src = anotherImg;
       });
     });
 
   };
   commandPhotos();
 
-  const calculate = () => {
+  // Calculator
+  const calc = (price = 100) => {
 
     const calcBlock = document.querySelector('.calc-block'),
+          calcType = document.querySelector('.calc-type'),
+          calcSquare = document.querySelector('.calc-square'),
+          calcDay = document.querySelector('.calc-day'),
+          calcCount = document.querySelector('.calc-count'),
+          totalValue = document.getElementById('total'),
           input = calcBlock.querySelectorAll('input');
 
+    const countSum = () => {
+      let total = 0,
+          countValue = 1,
+          dayValue = 1;
+      const typeValue = calcType.options[calcType.selectedIndex].value,
+            squareValue = +calcSquare.value;
+
+      if (calcCount.value > 1) {
+        countValue += (calcCount.value - 1) / 10;
+      }
+
+      if (calcDay.value && calcDay.value < 5) {
+        dayValue *= 2;
+      } else if (calcDay.value && calcDay.value < 10) {
+        dayValue *= 1.5;
+      }
+
+      if (typeValue && squareValue) {
+        total = price * typeValue * squareValue * countValue * dayValue;
+      }
+
+      totalValue.textContent = total;
+    };
+
     input.forEach((item) => {
-      item.addEventListener('input', () => {
-        item.value = input.value.replace(/\D/g, '');
+      item.addEventListener('input', (event) => {
+        event.target.value = event.target.value.replace(/\D/g, '');
       });
+    });
+
+    calcBlock.addEventListener('change', (event) => {
+
+      const target = event.target;
+      if (target.matches('select') || target.matches('input')) {
+        countSum();
+      }
+
     });
 
 
 
   };
-  calculate();
+  calc(100);
+
+
 
 });
